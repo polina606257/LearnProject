81a543cc80f1a13b1e6717c2525b94cb0641f94f
Polina Shcherbinina <polina.shcherbinina@gmail.com>
2024-08-31 16:56:10 +0300

    image glide

--- recycler/src/main/java/com/example/recycler/ui/ArtworkAdapter.kt
+++ recycler/src/main/java/com/example/recycler/ui/ArtworkAdapter.kt
@@ -23,7 +23,7 @@ class ArtworkAdapter : ListAdapter<BaseItem, RecyclerView.ViewHolder>(BaseDiffUt
     class ViewHolderArtwork(val binding: ArtworkItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(artwork: Artwork) {
             binding.textView.text = artwork.title
-            Glide.with(binding.root.context)
+            Glide.with(binding.root)
                 .load("${BASE_URL}iiif/2/${artwork.image_id}/full/843,/0/default.jpg")
                 .centerCrop()
                 .into(binding.image)
